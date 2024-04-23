c085006df3efa4a2352cf664dc09a60bb7f9f3f0
Bray Styven Henao Lopez <bray.henao.external@atos.net>
2024-04-23 15:45:10 +0100

    Añadido metodo POST, refactorizado los datos de usuario a una clase propia

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,23 +1,39 @@
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Usuario } from '../entitys/Usuario.entity';
+import { RouterModule , Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,HttpClientModule, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
-  userName: string = "";
-  email: string = "";
-  password: string = "";
+  usuario:Usuario;
 
-  registerUser() {
-    localStorage.setItem("Username", this.userName);
-    localStorage.setItem("email", this.email);
-    localStorage.setItem("password", this.password);
+  constructor(private http: HttpClient, private router: Router) {
+    this.usuario = new Usuario();
+  }
+
+  onRegister() {
+    debugger;
+    const headers = new HttpHeaders();
+    const body = {userName: this.usuario.userName,email: this.usuario.email, password: this.usuario.password}
+    this.http.post<any>("https://6627c6e6b625bf088c09bcf7.mockapi.io/api/v1/virtualclass/users", body, { headers }).subscribe((res:any)=>{
+      if (res != null) {
+        alert("Usuario registrado.");
+        // Colocar la pagina a la que envia al iniciar sesion.
+        // this.router.navigateByUrl("/usuario")
+      } else {
+        alert(res.message)
+      }
+    }, error => {
+      alert("Error from API")
+    })
   }
 
 }
