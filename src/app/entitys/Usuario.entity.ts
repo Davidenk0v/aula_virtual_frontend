98d1c71c60fe091bfc56105349c318020052dabb
Bray Styven Henao Lopez <bray.henao.external@atos.net>
2024-04-23 16:17:48 +0100

    Implementacion sin testear GET user, se separo los datos de usuario a una clase propia

---
+++ src/app/entitys/Usuario.entity.ts
@@ -0,0 +1,17 @@
+export class Usuario {
+
+    userName: string;
+    firstName: string;
+    lastName: string;
+    email:string;
+    password:string;
+  
+    constructor() {
+      this.userName = "";
+      this.firstName = "";
+      this.lastName = "";
+      this.email = "";
+      this.password = "";
+    }
+  
+  }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,27 @@
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Usuario } from '../entitys/Usuario.entity';
+import { RouterModule , Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,HttpClientModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  Username: string = "";
-  password: string = "";
+  usuario: Usuario;
   remenberMe: boolean = false;
 
-  constructor() {
+  constructor(private http: HttpClient, private router: Router) {
+    this.usuario = new Usuario();
+    this.onInit();
+  }
+
+  onInit() {
     if (localStorage.getItem("remenberMe") == null) {
       localStorage.setItem("remenberMe", "false");
     } else {
@@ -25,20 +32,33 @@ export class LoginComponent {
         this.remenberMe = false;
       }
     }
-
   }
 
   /**
    * Metodo que recoge la informacion del usuario y guarda si se debe recordar la sesion o no para iniciar automaticamente.
    * Los dato actualmente son almacenados localmente.
    * 
-   * TODO implementar llamada a la API con la verificacion del usuario.
+   * TODO Implementacion sin testear del GET
    */
-  loginUser() {
-    console.log(this.Username + this.password)
-    localStorage.setItem("Username", this.Username);
+  OnLogin() {
+    const headers = new HttpHeaders();
+    const body = {userName: this.usuario.userName, password: this.usuario.password}
+    this.http.post<any>("https://6627ba09b625bf088c097e87.mockapi.io/api/v1/users/user/", body, { headers }).subscribe((res:any)=>{
+      alert(res)
+      if (res != null) {
+        alert("Login Success");
+        // Colocar la pagina a la que envia al iniciar sesion.
+        // this.router.navigateByUrl("/usuario")
+      } else {
+        alert(res.message)
+      }
+    }, error => {
+      alert("Error from API")
+    })
+
+    localStorage.setItem("Username", this.usuario.userName);
     // Implementar un metodo para codificar la contraseña para almacenarla localmente?
-    localStorage.setItem("password", this.password);
+    localStorage.setItem("password", this.usuario.password);
     if (this.remenberMe) {
       localStorage.setItem("remenberMe", "true");
     } else {
