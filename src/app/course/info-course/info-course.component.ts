04dcf17282c8b21726acfdfc59abbb8fde0fb466
Rodrigo Sucapuca Alaca (ext) <rodrigo.sucapuca.external@eviden.com>
2024-04-24 16:01:38 +0100

    mejorando el componente  info-course del componente course para obtener las variables desde el .ts de propio componente

--- src/app/course/info-course/info-course.component.ts
+++ src/app/course/info-course/info-course.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './info-course.component.css'
 })
 export class InfoCourseComponent {
-  
+  name: String = "Matematicas: de Cero a Experto"
+  score: number = 4;
+  description:String ="Curso para aprender Matematicas fundamentales...";
+  number_ratings:String = "8.000.000";
+  creator:String ="Elon Musk";
+  start_date: String = "11/09/2024";
+  finish_date:String = "25/05/2025";
 }
