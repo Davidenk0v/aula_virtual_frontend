b019bf8fa7d6c030d66a9eef37182ed8958cc746
Foxssj <nicanorgonzalez2003@gmail.com>
2024-04-25 16:20:39 +0100

    feat: added more stylised versions of components and a carousel function

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -44,33 +44,95 @@ export class UserProfileComponent {
     { className: 'clase 8', description: 'test 1' },
     { className: 'clase 9', description: 'test 2' },
     { className: 'clase 10', description: 'test 2' },
-
-
-
   ];
 
+  classesChantity = 3;
+  currentSlideProgress = 0;
+  totalClassesProgress = this.classes.length;
 
-  currentSlide = 0
-  classesChantity = 3
-  totalClasses = this.classes.length
-  
-
-  changeSlideRight(){
-    if (this.currentSlide < this.totalClasses - this.classesChantity && this.currentSlide > -1) {
-      this.currentSlide = this.currentSlide + this.classesChantity;      
+  changeSlideRightProgress() {
+    if (
+      this.currentSlideProgress <
+        this.totalClassesProgress - this.classesChantity &&
+      this.currentSlideProgress > -1
+    ) {
+      this.currentSlideProgress =
+        this.currentSlideProgress + this.classesChantity;
     } else {
-      this.currentSlide = this.currentSlide = 0
-    }    
+      this.currentSlideProgress = this.currentSlideProgress = 0;
+    }
   }
 
-  changeSlideLeft(){
-    if (this.currentSlide <= this.totalClasses - this.classesChantity && this.currentSlide > 0) {
-      this.currentSlide = this.currentSlide - this.classesChantity;      
+  changeSlideLeftProgress() {
+    if (
+      this.currentSlideProgress <=
+        this.totalClassesProgress - this.classesChantity &&
+      this.currentSlideProgress > 0
+    ) {
+      this.currentSlideProgress =
+        this.currentSlideProgress - this.classesChantity;
     } else {
-      this.currentSlide = this.currentSlide = 0
-    }  
+      this.currentSlideProgress = this.currentSlideProgress = 0;
+    }
   }
 
+  currentSlideSaved = 0;
+  totalClassesSaved = this.classes.length;
+
+  changeSlideRightSaved() {
+    if (
+      this.currentSlideSaved <
+        this.totalClassesSaved - this.classesChantity &&
+      this.currentSlideSaved > -1
+    ) {
+      this.currentSlideSaved =
+        this.currentSlideSaved + this.classesChantity;
+    } else {
+      this.currentSlideSaved = this.currentSlideSaved = 0;
+    }
+  }
+
+  changeSlideLeftSaved() {
+    if (
+      this.currentSlideSaved <=
+        this.totalClassesSaved - this.classesChantity &&
+      this.currentSlideSaved > 0
+    ) {
+      this.currentSlideSaved =
+        this.currentSlideSaved - this.classesChantity;
+    } else {
+      this.currentSlideSaved = this.currentSlideSaved = 0;
+    }
+  }
+
+  currentSlideComplete = 0;
+  totalClassesComplete = this.classes.length;
+
+  changeSlideRightComplete() {
+    if (
+      this.currentSlideComplete <
+        this.totalClassesComplete - this.classesChantity &&
+      this.currentSlideComplete > -1
+    ) {
+      this.currentSlideComplete =
+        this.currentSlideComplete + this.classesChantity;
+    } else {
+      this.currentSlideComplete = this.currentSlideComplete = 0;
+    }
+  }
+
+  changeSlideLeftComplete() {
+    if (
+      this.currentSlideComplete <=
+        this.totalClassesComplete - this.classesChantity &&
+      this.currentSlideComplete > 0
+    ) {
+      this.currentSlideComplete =
+        this.currentSlideComplete - this.classesChantity;
+    } else {
+      this.currentSlideComplete = this.currentSlideComplete = 0;
+    }
+  }
 
   showPopup(): boolean {
     return (this.popupShown = true);
