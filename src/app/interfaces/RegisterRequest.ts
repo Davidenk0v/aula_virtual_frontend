1f001dba39148e57b6db352d8f5b577317c112b7
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-04-25 13:50:50 +0100

    refactor login and register done

--- src/app/interfaces/RegisterRequest.ts
+++ src/app/interfaces/RegisterRequest.ts
@@ -1,10 +1,12 @@
+import { Address } from "./Address";
+
 export interface RegisterRequest {
 
-    userName: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    address: string;
-    password: string;
+    userName?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    address?: Address;
+    password?: string;
   
   }
