487e5aa422ff7211b946448e73a28165f37bad5f
Rodrigo Sucapuca Alaca (ext) <rodrigo.sucapuca.external@eviden.com>
2024-04-26 11:28:04 +0100

    insertando el componente list-task al componente course

--- src/app/course/topic/topic.component.ts
+++ src/app/course/topic/topic.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListTaskComponent } from '../list-task/list-task.component';
 @Component({
   selector: 'app-topic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ListTaskComponent],
   templateUrl: './topic.component.html',
   styleUrl: './topic.component.css'
 })
 export class TopicComponent {
-  items: string[] = ['operaciones basicas', 'raices', ' trigometria','operaciones basicas', 'raices', ' trigometria'];
+
+  lessons: string[] = ['operaciones basicas', 'raices', ' trigometria','operaciones basicas', 'raices', ' trigometria'];
+
   topic: string = "Matematicas";
+
   selectedComponent: string = "";
+  showListTask: boolean = false;
 
   showComponent(item: string) {
     this.selectedComponent = item; 
-  }
+  } 
 }
