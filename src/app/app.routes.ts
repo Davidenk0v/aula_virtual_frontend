df2f3d355e1b3fc4cfe212bfb491d9a09b7b501f
Foxssj <nicanorgonzalez2003@gmail.com>
2024-04-29 16:58:53 +0100

    feat: created mostly of the view from the creation of a course

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { HomeComponent } from './home/home.component';
 import { CarouselComponentComponent } from './carousel-component/carousel-component.component';
+import { CreateCourseComponent } from './create-course/create-course.component';
 
 export const routes: Routes = [
     { path: "userProfile", component: UserProfileComponent},
     { path: "homeScreen", component: HomeComponent},
-    { path: "test", component: CarouselComponentComponent},
+    { path: "createCourse", component: CreateCourseComponent},
 ];
