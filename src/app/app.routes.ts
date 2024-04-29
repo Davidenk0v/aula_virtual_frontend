d41252a4aa2123ac7e86a6b103426413b31c973f
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-04-29 09:05:19 +0100

    ready for add footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { RegisterComponent } from './pages/register/register.component';
 export const routes: Routes = [
     {path:"", redirectTo:"/login", pathMatch:"full"},
     {path:"login", component:LoginComponent},
-    {path:"register", component:RegisterComponent},
+    {path:"register", component:RegisterComponent}
 
 ];
