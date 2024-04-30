c2c9d1500a56d5d68a650a9c90aef3d9fc2753a0
Foxssj <nicanorgonzalez2003@gmail.com>
2024-04-30 11:51:08 +0100

    feat: added the view to create a course, later the api will be implemeted

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { classContentI } from '../modelos/classContent.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-course',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-course.component.html',
   styleUrl: './create-course.component.css'
 })
 export class CreateCourseComponent {
+
+
+  // Este objeto es el que luego se mandrá a la base de datos
+  courseInfo: classContentI = {
+    name: "",
+    description: "",
+    startDate: new Date(1999,1,1),
+    finishDate: new Date(2000,1,1),
+    pago: 0
+  }
+
+  showData() {
+    console.log(this.courseInfo);
+  }
 
 }
