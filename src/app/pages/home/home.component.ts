217ba4f36664e5ad03336955a43c5f0011007cca
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-05-13 08:30:44 +0100

    carousel implementation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { CarouselComponentComponent } from '../../components/carousel-component/carousel-component.component';
 import { AsyncPipe } from '@angular/common';
 import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
-
-import { RouterModule } from '@angular/router';
 import { CourseService } from '../../services/courses/course.service';
+import { RouterModule } from '@angular/router';
+import { ImgMoveComponent } from "../../components/img-move/img-move.component";
 import { Course } from '../../interfaces/Course';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CarouselComponentComponent, AsyncPipe, ErrorMessageComponent, RouterModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CarouselComponentComponent, CarouselComponentComponent, AsyncPipe, ErrorMessageComponent, RouterModule, ImgMoveComponent]
 })
 export class HomeComponent {
 
