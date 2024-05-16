971cbf479e71d22bf5bff61b5e1ec0c54c8877e1
JoaoCM102 <joao.lima.external@atos.net>
2024-05-16 08:03:37 +0100

    commit

--- src/app/pages/zoom/zoom/zoom.component.ts
+++ src/app/pages/zoom/zoom/zoom.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { TeacherComponent } from "../teacher/teacher.component";
+import { StudentComponent } from "../student/student.component";
 @Component({
     selector: 'app-zoom',
     standalone: true,
     templateUrl: './zoom.component.html',
     styleUrl: './zoom.component.css',
-    imports: [TeacherComponent]
+    imports: [TeacherComponent, StudentComponent]
 })
 export class ZoomComponent {
 
+    role:string |  undefined
+    constructor(){}
 }
