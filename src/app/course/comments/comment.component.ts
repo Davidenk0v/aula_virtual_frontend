2c15e00560cc06a0a528b325ff7bd642f799fcf1
Nicanor Gonzalez Leon <nicanor.gonzalez.external@atos.net>
2024-05-20 15:42:13 +0100

    feat: separated the comments in a separated component to reduce code duplication

---
+++ src/app/course/comments/comment.component.ts
@@ -0,0 +1,140 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ListTaskComponent } from '../list-task/list-task.component';
+import { PaymentComponent } from '../payment/payment.component';
+import { CommentI } from '../../interfaces/Comment';
+import { AuthService } from '../../services/auth/auth.service';
+import { CommentService } from '../../services/comments/comments.service';
+import { JwtService } from '../../services/jwt/jwt.service';
+import { ProfileService } from '../../services/profile.service';
+
+@Component({
+  selector: 'app-comment',
+  standalone: true,
+  templateUrl: './comment.component.html',
+  imports: [
+    PaymentComponent,
+    FormsModule,
+    RouterModule,
+    ListTaskComponent,
+    ReactiveFormsModule,
+  ],
+})
+export class CommentComponent {
+  @Input() courseId?: number;
+  loggeIn: boolean = false;
+  currentDate =
+    new Date().getFullYear() +
+    '-0' +
+    new Date().getMonth() +
+    '-' +
+    new Date().getUTCDate();
+  newComment = '';
+  coments?: any;
+  isEditingAComment?: boolean = false;
+  clickedComment = 0;
+  editedComment = '';
+  nameUser: any;
+  idUser = '';
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private commentService: CommentService,
+    private jwtService: JwtService,
+    private user: ProfileService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.courseId = Number(this.activateRoute.snapshot.paramMap.get('id'));
+    this.getComents(this.courseId);
+    this.nameUser = this.jwtService.getNameFromToken();
+    this.idUser = this.jwtService.getIdFromToken();
+    this.isLogged();
+  }
+
+  getComents(courseId: number) {
+    this.commentService.getAllComments(courseId).subscribe({
+      next: (comments: any) => {
+        this.coments = comments;
+        this.coments.sort((a: any, b: any) => b.idComment - a.idComment);
+        console.log('Comments:', this.coments);
+      },
+      error: (error: Error) => {
+        console.error('Error fetching comments:', error);
+      },
+    });
+  }
+
+  getUsername(): string {
+    return this.jwtService.getUsernameFromToken();
+  }
+
+  addNewComent(idCourse?: number) {
+    let comment: CommentI = {
+      text: this.newComment,
+      date: this.currentDate,
+      user: {
+        firstname: this.nameUser,
+        username: this.jwtService.getEmailFromToken(),
+      },
+    };
+
+    this.commentService.postComment(idCourse!, this.idUser, comment).subscribe({
+      next: (cita: any) => {},
+      error: (userData: any) => {
+        console.log(userData);
+      },
+      complete: () => {
+        this.coments.unshift(comment);
+      },
+    });
+  }
+
+  toggleEdit(id: number) {
+    this.clickedComment = id;
+    this.isEditingAComment = !this.isEditingAComment;
+    this.editedComment = this.coments[id - 1].text;
+  }
+
+  confirmCommentChange(id: number, newComment: string) {
+    this.coments[id - 1].text = newComment;
+    this.commentService.editComment(this.coments[id - 1], id).subscribe({
+      next: (data: any) => {
+        console.info(data);
+      },
+      error: (userData: any) => {
+        console.log(userData);
+      },
+      complete: () => {
+        console.info('Completo');
+        if (this.coments.IdComment) {
+          this.getSubjects(this.coments.IdComment);
+        }
+      },
+    });
+    this.toggleEdit(id);
+  }
+  getSubjects(IdComment: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  isLogged() {
+    this.authService.loggedIn$.subscribe({
+      next: (logged: any) => {
+        this.loggeIn = logged;
+        if (!this.loggeIn) {
+          this.loggeIn =
+            sessionStorage.getItem('loggin') == 'true';
+        }
+      },
+      error: (error: Error) => {
+        console.error('Error fetching comments:', error);
+      },
+      complete: () => {
+        console.info('Request completed');
+      },
+    });
+  }
+}
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -3,8 +3,6 @@ import { PaymentComponent } from './payment/payment.component';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Course } from '../interfaces/Course';
 import { CourseService } from '../services/courses/course.service';
-import { CommentService } from '../services/comments/comments.service';
-import { CommentI } from '../interfaces/Comment';
 import { ListTaskComponent } from './list-task/list-task.component';
 import { Subject } from '../interfaces/Subject';
 import {
@@ -17,6 +15,7 @@ import { SubjectsService } from '../services/subjects/subjects.service';
 import { JwtService } from '../services/jwt/jwt.service';
 import { ProfileService } from '../services/profile.service';
 import { AuthService } from '../services/auth/auth.service';
+import { CommentComponent } from './comments/comment.component';
 @Component({
   selector: 'app-course',
   standalone: true,
@@ -28,6 +27,7 @@ import { AuthService } from '../services/auth/auth.service';
     RouterModule,
     ListTaskComponent,
     ReactiveFormsModule,
+    CommentComponent
   ],
 })
 export class CourseComponent {
@@ -38,7 +38,6 @@ export class CourseComponent {
     private activateRoute: ActivatedRoute,
     private courseService: CourseService,
     private formBuild: FormBuilder,
-    private commentService: CommentService,
     private jwtService: JwtService,
     private user: ProfileService,
     private authService:AuthService
@@ -52,114 +51,24 @@ export class CourseComponent {
   userData= {
   };
   submenuAbierto = false;
-  currentDate = new Date().getFullYear() + "-0" + new Date().getMonth() + "-" + new Date().getUTCDate()  ; 
-  newComment = ""
-  coments?: any;
+  
   subjectId?: number;
-  loggeIn: boolean = false;
-  isEditingAComment?: boolean = false;
   subjectForm = this.formBuild.group({
     name: ['', [Validators.required]],
     description: ['', [Validators.required]],
   });
 
 
-  getComents(courseId: number) {
-    this.commentService.getAllComments(courseId).subscribe({
-      next: (comments) => {
-        this.coments = comments;
-        this.coments.sort((a: any, b: any) => a.idComment - b.idComment);
-        console.log('Comments:', this.coments);
-      },
-      error: (error) => {
-        console.error('Error fetching comments:', error);
-      }
-    });
-  }
-
-  getUsername(): string{ 
-    return this.jwtService.getUsernameFromToken()
-  }
-  
-  addNewComent(idCourse?: number) {
-    let comment: CommentI = { 
-      text: this.newComment,
-      date: this.currentDate, 
-      user: { 
-        firstname: this.nameUser,
-        username: this.jwtService.getEmailFromToken()
-      }
-      
-    };
-
-    this.commentService.postComment(idCourse!, this.idUser, comment)
-    .subscribe({
-      next: (cita) => {
-      },
-      error: (userData) => {
-        console.log(userData);
-      },
-      complete: () => {
-        this.coments.unshift(comment)
-      },
-    })
-  }
-
-  clickedComment = 0
-  editedComment = ""
-
-  toggleEdit(id: number) {
-    this.clickedComment = id
-    this.isEditingAComment = !this.isEditingAComment
-    this.editedComment = this.coments[id - 1].text
-  }
 
 
-  confirmCommentChange(id: number, newComment: string) {
-    this.coments[id - 1].text = newComment
-    this.commentService.editComment(this.coments[id-1], id).subscribe({
-      next: (data) => {
-        console.info(data);
-      },
-      error: (userData) => {
-        console.log(userData);
-      },
-      complete: () => {
-        console.info('Completo');
-        if (this.coments.IdComment) {
-          this.getSubjects(this.coments.IdComment);
-        }
-      },
-    })
-    this.toggleEdit(id)
-  }
 
 
   ngOnInit(): void {
     this.courseId = Number(this.activateRoute.snapshot.paramMap.get('id'));
     this.getCourseInfo(this.courseId);
     this.getSubjects(this.courseId);
-    this.getComents(this.courseId)
     this.nameUser = this.jwtService.getNameFromToken();
     this.idUser = this.jwtService.getIdFromToken()
-    this.isLogged()
-  }
-
-  isLogged(){
-    this.authService.loggedIn$.subscribe({
-      next: (logged) => {
-        this.loggeIn = logged;
-        if(!this.loggeIn){
-          this.loggeIn = sessionStorage.getItem('loggin') == 'true' ? true : false;
-        }
-      },
-      error: (error) => {
-        console.error('Error fetching comments:', error);
-      },
-      complete: () => {
-        console.info('Request completed');
-      },
-    });
   }
 
 
